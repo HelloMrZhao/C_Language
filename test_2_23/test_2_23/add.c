c1c62d340d559b012ec8ec7c50e8ca4faa69e058
HelloMrZhao <406013226@qq.com>
2023-02-25 08:44:52 +0800

    C语言编写

---
+++ test_2_23/test_2_23/add.c
@@ -0,0 +1,49 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//计算机语言-写程序-解决生活问题
+//必须有能力来描述生活问题
+//C语言-浮点数
+//购物商城-上架商品，价格-15.6元-小数  1.56*10^1
+// -整型
+// 年龄-50
+
+//char  字符数据类型
+//short 短整型
+//int   整型
+//long   长整型
+//long long 更长的整型
+//float 单精度浮点数
+//double 双精度浮点数
+
+//例：描述一个字符'a'
+
+#include <stdio.h>
+//int main()
+//{
+//	//字符型
+//	//数据物料存储需要空间，字符存储也需要空间  
+//	char ch='a';   //char类型给字符a创建一个空间叫ch，存放字符'a',将字符a存放入ch的变量空间中
+//    //整型
+//	int age = 20;
+//	short num = 10; //短整型
+//	//long 长整型
+//	//long long 更长的整型
+//	// 小数类型
+//	//单精度浮点型
+//	float weight = 55.5;
+//	//双精度浮点型
+//	double d = 0.0;
+//	
+//	return 0;
+//}
+
+int main()
+{
+	printf("hello\n");
+	printf("比特\n");
+
+	return 0;
+}
+
+
+
+
---
+++ test_2_23/test_2_23/test.c
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+//int main()
+//{
+//	int a = 0 ;
+//	scanf("%d", &a);
+//	printf("a=%d\n", a);
+//	//printf("Hello world\n");
+//	return 0;
+//}
+////不安全函数
+////scanf
+////strcpy
+////strcat
+////
+////newc++file.cpp  .c文件都拷贝自这个文件
