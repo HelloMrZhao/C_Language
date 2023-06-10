429e4aa73760564db1cfd97bcb5f68fea596f3d7
HelloMrZhao <406013226@qq.com>
2023-06-10 22:29:49 +0800

    memcmp内存比较\memset内存设置

---
+++ test_6_10/test_6_10/test.c
@@ -0,0 +1,28 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+
+//int memcmp(const void* ptr1,const void* ptr2,size_t num);
+// 比较从ptr1和ptr2指针开始的num个字节
+//memcmp内存比较
+//int main()
+//{
+//	int arr1[] = { 1,2,3,4,5 };
+//	int arr2[] = { 1,2,3,4,0x11223305 };
+//	int ret  = memcmp(arr1, arr2,16);
+//	printf("%d\n", ret);
+//	return 0;
+//}
+
+
+//memset内存设置
+//int main()
+//{
+//	int arr[] = { 1,2,3,4,5 };
+//	memset(arr, 0, 20);//将20个字节的内存全部设置成0
+//	//memset是以字节为单位来初始化内存单元的
+// 
+//	return 0;
+// }
+
+
